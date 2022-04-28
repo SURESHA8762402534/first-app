2f30200ad01f93dc341700a5145bcf0a472cef05
suresh <sureshamahendra.gmail.com>
2022-04-28 15:44:32 +0530

    again

--- src/componets/Country.tsx
+++ src/componets/Country.tsx
@@ -1,66 +1,98 @@
-import { Avatar, Button, Grid, Paper, Table, TableCell, TableHead, TableRow, Typography } from '@mui/material';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  Avatar,
+  Button,
+  Grid,
+  Paper,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Country = () => {
-    const [data, setData]: any = useState();
-    const navigate = useNavigate();
+  const [data, setData]: any = useState();
+  const navigate = useNavigate();
 
-    const country: any = useLocation().state;
+  const country: any = useLocation().state;
 
-    const handeleData = async () => {
-        try {
-            const res = await axios.get(`https://restcountries.com/v3.1/name/${country}`);
-            console.log(res);
-            setData(res.data[0])
-        }
-        catch (e) {
-            console.log('error');
-        }
+  const handeleData = async () => {
+    try {
+      const res = await axios.get(
+        `https://restcountries.com/v3.1/name/${country}`
+      );
+      console.log(res);
+      setData(res.data[0]);
+    } catch (e) {
+      console.log("error");
     }
+  };
 
+  useEffect(() => {
+    handeleData();
+  }, [country]);
 
-    useEffect(() => {
-        handeleData()
-    }, [country]);
+  return (
+    <div>
+      <Button
+        data-testid="home"
+        sx={{ m: 3 }}
+        variant="contained"
+        onClick={() => navigate("/")}
+      >
+        HOME
+      </Button>
+      {data ? (
+        <Paper elevation={24} sx={{ width: "30vw", ml: "35vw", mt: 5, p: 2 }}>
+          <Typography
+            variant="h5"
+            sx={{ textAlign: "center", m: 2, textDecoration: "underline" }}
+          >
+            COUNTRY DETAILS
+          </Typography>
+          <Grid container>
+            <Grid sx={{ float: "left" }}>
+              <Typography sx={{ m: 1 }} variant="h5">
+                <b>{country} </b>
+              </Typography>
+              <Typography sx={{ m: 1 }} variant="h6">
+                Capital city : {data.capital[0]}
+              </Typography>
+              <Typography sx={{ m: 1 }} variant="h6">
+                Population : {data.population}
+              </Typography>
+              <Typography sx={{ m: 1 }} variant="h6">
+                Latitude : {data.latlng[0]}
+              </Typography>
+              <Typography sx={{ m: 1 }} variant="h6">
+                Longitude : {data.latlng[1]}
+              </Typography>
+            </Grid>
+            <Grid sx={{ float: "right" }}>
+              <Avatar
+                variant="rounded"
+                src={data.flags.png}
+                sx={{ width: "5rem", height: "5rem", ml: 10, mt: 5 }}
+              />
+            </Grid>
+          </Grid>
+          <Typography sx={{ textAlign: "center" }}>
+            <Button
+              data-testid="capitalweather"
+              sx={{ m: 2 }}
+              variant="contained"
+              onClick={() => navigate("/weather", { state: data.capital[0] })}
+            >
+              CAPITAL WEATHER
+            </Button>
+          </Typography>
+        </Paper>
+      ) : null}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <Button data-testid='home' sx={{ m: 3 }} variant='contained' onClick={() => navigate('/')}>HOME</Button>
-            {data ?
-                <Paper elevation={24} sx={{ width: '30vw', ml: '35vw', mt: 5, p: 2 }}>
-                    <Typography variant='h5' sx={{ textAlign: 'center', m: 2, textDecoration: 'underline' }}>COUNTRY DETAILS</Typography>
-                    <Grid container>
-                        <Grid sx={{ float: 'left' }}>
-                            <Typography sx={{ m: 1 }} variant='h5'>
-                                <b>{country} </b>
-                            </Typography>
-                            <Typography sx={{ m: 1 }} variant='h6'>
-                                Capital city : {data.capital[0]}
-                            </Typography>
-                            <Typography sx={{ m: 1 }} variant='h6'>
-                                Population : {data.population}
-                            </Typography>
-                            <Typography sx={{ m: 1 }} variant='h6'>
-                                Latitude : {data.latlng[0]}
-                            </Typography>
-                            <Typography sx={{ m: 1 }} variant='h6'>
-                                Longitude : {data.latlng[1]}
-                            </Typography>
-                        </Grid>
-                        <Grid sx={{ float: 'right' }}>
-                            <Avatar variant='rounded' src={data.flags.png} sx={{ width: '5rem', height: '5rem', ml: 10, mt: 5 }} />
-                        </Grid>
-                    </Grid>
-                    <Typography sx={{ textAlign: 'center' }}>
-                        <Button data-testid='capitalweather' sx={{ m: 2 }} variant='contained' onClick={() => navigate('/weather', { state: data.capital[0] })}>CAPITAL WEATHER</Button>
-                    </Typography>
-                </Paper>
-                :
-                null}
-        </div>
-    )
-}
-
-export default Country
+export default Country;
--- src/componets/Nav.tsx
+++ src/componets/Nav.tsx
@@ -1,38 +1,44 @@
-import { AppBar, Button, TextField, Toolbar, Typography } from '@mui/material';
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { AppBar, Button, TextField, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
-    const [input, setInput] = useState('');
-    const navigate = useNavigate();
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
-    const handelSubmit = (e:any)=>{
-        e.preventDefault();
-        setInput('');
-        navigate('/country',{state:input})
-        console.log(input);
-    }
+  const handelSubmit = (e: any) => {
+    e.preventDefault();
+    setInput("");
+    navigate("/country", { state: input });
+    console.log(input);
+  };
 
   return (
     <div>
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography variant='h5' sx={{flexGrow:1}}>
-                    WETHER REPORT
-                </Typography>
-                <form action="" onSubmit={handelSubmit}>
-                    <input
-                    style={{height:'2rem'}}
-                     value={input} 
-                     placeholder='enter country name' 
-                     onChange={(e:any)=>setInput(e.target.value)}
-                     />
-                     <button style={{height:'2.4rem'}} disabled={!input}  type='submit'>SEARCH</button>
-                </form>
-            </Toolbar>
-        </AppBar>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" sx={{ flexGrow: 1 }}>
+            WETHER REPORT
+          </Typography>
+          <form action="" onSubmit={handelSubmit}>
+            <input
+              style={{ height: "2rem" }}
+              value={input}
+              placeholder="enter country name"
+              onChange={(e: any) => setInput(e.target.value)}
+            />
+            <button
+              style={{ height: "2.4rem" }}
+              disabled={!input}
+              type="submit"
+            >
+              SEARCH
+            </button>
+          </form>
+        </Toolbar>
+      </AppBar>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
